f8c5e46b5bc3dc12d7e72c1f9cbf31cc507452a8
Radhika Shukla <146022535+radhishukla@users.noreply.github.com>
2024-03-18 00:45:23 +0530

    Initial commit

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/name")
-def name():
-  return "Rads"
+@app.route("/")
+def hello_world():
+  return render_template("index.html",title="Hello")
 
 
-@app.route("/age)
-           def age():
-             return "20"
+@app.route("/name)
+           def name():
+             return "Rads"
 
+@app.route("/age")
+def age():
+  return "20"
+  
 @app.route("/hello")
 def hello():
   return "hello"
